e3326434e6fb43423e62531c731ae484fae150de
pengqb <xiaobing525@163.com>
2015-10-10 17:17:26 +0800

    2222
    1111

---
+++ main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  EGL
+//
+//  Created by pengqb  on 15/10/10.
+//  Copyright © 2015年 pengqb . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+        
+        
+        
+    }
+}
